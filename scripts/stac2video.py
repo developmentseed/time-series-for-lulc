59aef3116c3df4d235ee109bdbfd636d05e466a7
Daniel Wiesmann <daniel@wiesmann.pt>
2023-05-04 10:27:32 +0100

    use tms tiles for video generation

--- scripts/stac2video.py
+++ scripts/stac2video.py
@@ -1,4 +1,7 @@
+import json
+
 import cv2
+import morecantile
 import numpy
 import pystac_client
 import stackstac
@@ -29,60 +32,61 @@ collection = "sentinel-s2-l2a-cogs"
 # collection = "landsat-c2l2-sr"
 
 epsg = 3857
-coordx, coordy = -985632, 4726291
-resolution = 60
+zoom = 12
+
+# Cretas, Greece
+coordx, coordy = 35.580521, 23.592116
+coordx, coordy = 23.47799, 38.18146
+# Sahara
+coordx, coordy = 7.89839, 26.90210
+# Australia
+# coordx, coordy = 121.41734, -32.81854
+
+# Portugal
+coordx, coordy = -9.15032, 38.72595
+
 size = 256
 
+tms = morecantile.tms.get("WebMercatorQuad")
 
-def gen_bounds(coordx, coordy, resolution, size):
-    step = resolution * size
-    for i in [0, 1]:
-        for j in [0, 1]:
-            yield {
-                "type": "Polygon",
-                "coordinates": [
-                    [
-                        [coordx + i * step, coordy + j * step],
-                        [coordx + (i + 1) * step, coordy + j * step],
-                        [coordx + (i + 1) * step, coordy - (j + 1) * step],
-                        [coordx + i * step, coordy - (j + 1) * step],
-                        [coordx + i * step, coordy + j * step],
-                    ]
-                ],
-            }
+first_tile = tms.tile(coordx, coordy, zoom)
+print(first_tile)
+
+tiles = []
+for i in range(2):
+    for j in range(2):
+        tiles.append(morecantile.Tile(first_tile.x + i, first_tile.y + j, first_tile.z))
 
 
-for geom in gen_bounds(coordx, coordy, resolution, size):
-    # Search for imagery that intersects with the bbox of the file. This
-    # bbox is required to be in lat/lon, so we use the original bounds.
+for tile in tiles:
     # start = "2017-06-01"
-    start = "2023-01-01"
+    start = "2022-01-01"
     search = catalog.search(
         collections=[collection],
-        intersects=transform_geom(f"EPSG:{epsg}", "EPSG:4326", geom),
+        bbox=tms.bounds(tile),
         datetime=f"{start}/2023-04-30",
     )
     items = search.get_all_items()
     print(f"Found {len(items)} items")
 
-    # Stack imagery in mexican bounds at 10m resolution.
+    # Stack imagery
     stack = stackstac.stack(
         items,
-        bounds=geomBounds(geom),
+        bounds=tms.xy_bounds(tile),
         snap_bounds=False,
         epsg=epsg,
-        resolution=resolution,
+        resolution=tms._resolution(tms.matrix(zoom=zoom)),
         dtype="uint16",
         fill_value=0,
         resampling=Resampling.bilinear,
     )
 
-    # Keep all bands with 10m or 20m resolution.
+    # RGB as BGR plus scene class for cloud masking
     stack = stack.sel(
         band=[
-            "B04",
-            "B03",
             "B02",
+            "B03",
+            "B04",
             "SCL",
         ]
     )
@@ -113,18 +117,20 @@ for geom in gen_bounds(coordx, coordy, resolution, size):
         composites_using_cloud_mask,
     )
 
-    filepath = "/home/tam/Desktop/videomap/videomap-{}-{}-{}-{}.mp4".format(
-        *geomBounds(geom)
-    )
+    filepath = f"/home/tam/Desktop/videomap/videomap-{tile.z}-{tile.x}-{tile.y}.mp4"
+
     out = cv2.VideoWriter(
         filepath, fourcc=cv2.VideoWriter_fourcc(*"mp4v"), fps=4, frameSize=(size, size)
     )
     for img in composites:
-        print("A")
-        rgb = (
-            numpy.clip(255 * img.data[:3] / 1500, 0, 255)
-            .astype("uint8")
-            .transpose(1, 2, 0)
-        )
-        out.write(rgb)
+        bgr = img.data[:3].transpose(1, 2, 0)
+        bgr = 255 * bgr / 3000
+        bgr = numpy.clip(bgr, 0, 255).astype("uint8")
+        out.write(bgr)
+
+    geojson = f"/home/tam/Desktop/videomap/videomap-{tile.z}-{tile.x}-{tile.y}.geojson"
+
+    with open(geojson, "w") as f:
+        json.dump(tms.feature(tile), f)
+
     out.release()
